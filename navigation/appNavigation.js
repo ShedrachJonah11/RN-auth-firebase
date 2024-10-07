ef18d0ee1817161b7b0af76dc4e1d6f630ce6736
“ShedrachJonah11” <ShedrachJonah11@gmail.com>
2024-10-07 15:35:24 +0100

    feat: add files structure

---
+++ navigation/appNavigation.js
@@ -0,0 +1,38 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import WelcomeScreen from "../screens/WelcomeScreen";
+import LoginScreen from "../screens/LoginScreen";
+import SignupScreen from "../screens/SignupScreen";
+import HomeScreen from "../screens/HomeScreen";
+
+const Stack = createNativeStackNavigator();
+
+export default function appNavigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          options={{ headerShown: false }}
+          component={WelcomeScreen}
+        />
+        <Stack.Screen
+          name="Login"
+          options={{ headerShown: false }}
+          component={LoginScreen}
+        />
+        <Stack.Screen
+          name="Signup"
+          options={{ headerShown: false }}
+          component={SignupScreen}
+        />
+        <Stack.Screen
+          name="Home"
+          options={{ headerShown: false }}
+          component={HomeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
